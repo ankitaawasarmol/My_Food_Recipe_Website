9b8b9d7af5764517ba7ae694ca59e53e611641b2
Ankita <ankitaawasarmol04@gmail.com>
2023-02-13 14:48:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, createContext, useState} from 'react';
+import './Components/style.css';
+import Meal from './Components/Meal';
+import Recipe from './Components/Recipe';
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import About from './Components/About';
+import Contact from './Components/Contact';
+
+import Login from './Components/Login';
+import Signup from './Components/Signup';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import app from "./Components/firebaseConfig";
+import {getAuth, onAuthStateChanged} from 'firebase/auth';
+
+export const UserContext=createContext({});
 
 function App() {
+  const auth=getAuth(app);
+  const[authenticatedUser,setAuthenticatedUser]=useState(null);
+
+  useEffect(()=>{
+    onAuthStateChanged(auth, (user)=>{
+      if(user){
+      
+        setAuthenticatedUser(user);
+      }else{
+        setAuthenticatedUser(null);
+      }
+    })
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <UserContext.Provider value={authenticatedUser}>
+     <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/About" element={<About/>}/>
+      <Route path="/Contactus" element={<Contact/>}/>
+      <Route path="/Login" element={<Login/>}/>
+      <Route path="/Signup" element={<Signup/>}/>
+      <Route  path="/Meal" element={<Meal/>}/>
+      <Route exact path="/:recipeId" element={<Recipe/>}/>
+      </Routes>
+      
+    </UserContext.Provider>
+  )
 }
 
 export default App;
---
+++ src/Components/firebaseConfig.jsx
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: "AIzaSyChSQNWUeLDA-7h2Hb5g0wTnzy8GkIXh3o",
+  authDomain: "foodrecipe-87064.firebaseapp.com",
+  projectId: "foodrecipe-87064",
+  storageBucket: "foodrecipe-87064.appspot.com",
+  messagingSenderId: "751992773857",
+  appId: "1:751992773857:web:d55f77814ae94c5b6a2ba0",
+  measurementId: "G-FCGMMTGYPH"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
